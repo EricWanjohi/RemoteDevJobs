b067085a4671ded05af586e8b5a600c53e4fd1c1
Eric W <ericwanjohi.ew@gmail.com>
2020-05-13 17:55:52 +0300

    - Create and implement RemoteAdapter.

--- app/src/main/java/ke/co/droidsense/remotedev/adapter/RemoteOkAdapter.java
+++ app/src/main/java/ke/co/droidsense/remotedev/adapter/RemoteOkAdapter.java
@@ -1,4 +1,66 @@
 package ke.co.droidsense.remotedev.adapter;
 
-public class RemoteOkAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import ke.co.droidsense.remotedev.R;
+import ke.co.droidsense.remotedev.model.RemoteOkResponse;
+
+public class RemoteOkAdapter extends RecyclerView.Adapter<RemoteOkAdapter.ViewHolder> {
+    //Member Variables...
+    private List<RemoteOkResponse> remoteOkResponses;
+    private Context context;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Create View item...
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //Create Object holder...
+
+        //Bind data
+
+        //Set item tag
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return remoteOkResponses.size();
+    }
+
+    //Create ViewHolder...
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        //Member Variables...
+        private TextView position;
+
+        //Constructor...
+        public ViewHolder(@NonNull View itemView) {
+            super( itemView );
+
+            //Initializations...
+            position = itemView.findViewById( R.id.remote_job_position );
+
+            //Set Click Listener.
+            itemView.setOnClickListener( this );
+        }
+
+        @Override
+        public void onClick(View view) {
+            //Handle click events...
+
+        }
+    }
 }
