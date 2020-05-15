cdc1462da7a23f7f03251fdc9231c7003603e30e
Eric W <ericwanjohi.ew@gmail.com>
2020-05-15 12:21:24 +0300

    - Add TextView items to ViewHolder and bind data to views in the Adapter. - Bind company logo ImageView item using Picasso library.

--- app/src/main/java/ke/co/droidsense/remotedev/adapter/RemoteOkAdapter.java
+++ app/src/main/java/ke/co/droidsense/remotedev/adapter/RemoteOkAdapter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -16,6 +19,8 @@ import ke.co.droidsense.remotedev.R;
 import ke.co.droidsense.remotedev.model.RemoteOkResponse;
 
 public class RemoteOkAdapter extends RecyclerView.Adapter<RemoteOkAdapter.ViewHolder> {
+    private static final int MAX_WIDTH = 50;
+    private static final int MAX_HEIGHT = 50;
     //Member Variables...
     private List<RemoteOkResponse> remoteOkResponses;
     private Context context;
@@ -41,7 +46,15 @@ public class RemoteOkAdapter extends RecyclerView.Adapter<RemoteOkAdapter.ViewHo
         RemoteOkResponse remoteOkResponse = remoteOkResponses.get( position );
 
         //Bind data
+        holder.company_name.setText( remoteOkResponse.getCompany() );
         holder.position.setText( remoteOkResponse.getPosition() );
+        holder.date_posted.setText( remoteOkResponse.getDate() );
+
+        //Bind Logo image using Picasso...
+        Picasso.get().load( remoteOkResponse.getLogo() )
+                .resize( MAX_WIDTH, MAX_HEIGHT )
+                .centerCrop()
+                .into( holder.company_logo );
 
         //Set item tag
         holder.itemView.setTag( remoteOkResponse );
@@ -56,14 +69,18 @@ public class RemoteOkAdapter extends RecyclerView.Adapter<RemoteOkAdapter.ViewHo
     //Create ViewHolder...
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         //Member Variables...
-        private TextView position;
+        private TextView company_name, position, date_posted;
+        private ImageView company_logo;
 
         //Constructor...
         public ViewHolder(@NonNull View itemView) {
             super( itemView );
 
             //Initializations...
+            company_name = itemView.findViewById( R.id.remote_job_company );
             position = itemView.findViewById( R.id.remote_job_position );
+            date_posted = itemView.findViewById( R.id.remote_job_date );
+            company_logo = itemView.findViewById( R.id.company_logo );
 
             //Set Click Listener.
             itemView.setOnClickListener( this );
